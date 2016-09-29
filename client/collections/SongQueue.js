617c544a7e10cfebdbe0b3b2a2717f83a6268537
spiritphyz <heytonyle@icloud.com>
2016-09-28 19:27:47 -0700

    Working on SongQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,8 +2,25 @@
 var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
-
   initialize: function() {
-  }
+    this.on('add', function(song) {
+      console.log(song);
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function () {
+    
+  },
+
 
 });
